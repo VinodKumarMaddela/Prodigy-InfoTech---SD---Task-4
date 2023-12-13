0eddfa7d28cad1e0576dfbebb91bac0822b1ac88
Vinod Kumar Maddela <151427845+VinodKumarMaddela@users.noreply.github.com>
2023-12-13 12:59:12 +0530

    Sudoku Solver

---
+++ Task4_SudokuSolver.java
@@ -0,0 +1,112 @@
+public class Task4_SudokuSolver 
+{
+
+public static void main(String [] args)
+{
+    int [][] sudokuGrid = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+
+    if(solveSudoku(sudokuGrid))
+    {
+        printSudoku(sudokuGrid);
+    }
+    else
+    {
+        System.out.println("No solution exists.");
+    }
+}
+private static boolean solveSudoku(int[][] grid)
+{
+    for(int row =0;row<9;row++)
+    {
+        for(int col =0 ;col<9;col++)
+        {
+            if(grid[row][col]==0)
+            {
+                for(int num=1;num<=9;num++)
+                {
+                    if(isValidMove(grid, row, col, num))
+                    {
+                        grid[row][col]=num;
+
+                        if(solveSudoku(grid))
+                        {
+                            return true;
+                        }
+                        grid[row][col]=0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+private static boolean isValidMove(int[][] grid, int row, int col, int num)
+{
+    return !usedInRow(grid, row, num) &&
+            !usedInColumn(grid, col, num) &&
+            !usedInBox(grid, row - row % 3, col - col % 3, num);
+}
+
+private static boolean usedInRow(int [][] grid, int row, int num)
+{
+    for(int col=0; col<9;col++)
+    {
+        if(grid[row][col]==num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+private static boolean usedInColumn(int [][] grid, int col, int num)
+{
+    for(int row =0; row<9;row++)
+    {
+        if(grid[row][col]==num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+private static boolean usedInBox(int[][] grid, int startRow, int startCol, int num)
+{
+    for(int row =0 ; row<3;row++)
+    {
+        for(int col=0;col<3;col++)
+        {
+            if(grid[row + startRow][col + startCol] == num)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+private static void printSudoku(int[][] grid)
+{
+    for(int row=0;row<9;row++)
+    {
+        for(int col=0;col<9;col++)
+        {
+            System.out.print(grid[row][col]+" ");
+        }
+        System.out.println();
+    }
+}    
+}
